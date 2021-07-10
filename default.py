53b87dc934ad391b2adf0a79eade316d361df45e
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-10 17:51:09 -0500

    Initial commit

--- default.py
+++ default.py
@@ -1,8 +1,9 @@
 # learning to being an expert #
 from bs4 import BeautifulSoup
 import requests
+import lxml
 
 url = requests.get('https://www.teamfortress.com/').text
-soup = BeautifulSoup(url, 'html')
-id = soup.find('id').text
-print(url.text())
+soup = BeautifulSoup(url, 'lxml')
+id = soup.find('id')
+print(id)
