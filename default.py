b22ab7d50486035c0c9d4527b2bd95016d85006a
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-11 10:49:26 -0500

    Initial commit

--- default.py
+++ default.py
@@ -6,5 +6,7 @@ import lxml
 url = requests.get('https://www.teamfortress.com/').text
 soup = BeautifulSoup(url, 'lxml')
 id_find = soup.find_all('div')
+href = soup.find_all('href')
 # text = id_find.id.text
-print(id_find)
+for href in id_find:
+    print(href)
