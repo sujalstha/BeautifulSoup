b683b7d782b985699eecaac6df904b2f40526269
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-11 22:11:19 -0500

    Initial commit

--- default.py
+++ default.py
@@ -8,5 +8,8 @@ soup = BeautifulSoup(url, 'lxml')
 id_find = soup.find_all('div')
 href = soup.find_all('href')
 # text = id_find.id.text
+
+all_text_tf_website = []
+
 for href in id_find:
-    print(href.text)
+    print(href)
