ed72902466a292810c43098e16958ffa9a720cc1
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-12 15:18:54 -0500

    Initial commit

--- default.py
+++ default.py
@@ -25,9 +25,11 @@ for lis in all_text_tf_website:
 
 
 def remove_items(test_list, item):
-
     res = [i for i in test_list if i != item]
-
     return res
 
 
+if __name__ == "__main__":
+    test_list = [1, 3, 4, 6, 5, 1]
+    item = 1
+    print("The original list is : " + str(test_list))
