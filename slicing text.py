0f6b2c14545f4fa99ce95d6eb595282ad8400114
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-15 15:54:27 -0500

    Initial commit

--- slicing text.py
+++ slicing text.py
@@ -1,2 +1,10 @@
-with open("tf2 website text.txt") as file:
-    file.readlines()
+import re
+
+with open("tf2 website text.txt", "w") as file:
+    final = re.sub("\.", ".\n")
+
+
+def remove_doublespace(string):
+    if '  ' not in string:
+       return string
+    return remove_doublespace(string.replace('  ',' '))
