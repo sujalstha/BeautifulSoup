256c3f19458f1405a729beddea4b3cc9f7f2495c
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-15 15:57:52 -0500

    Initial commit

--- slicing text.py
+++ slicing text.py
@@ -3,7 +3,7 @@ import io
 
 web_lis = []
 
-with io.open("tf2 website text.txt", "w") as file:
+with io.open("tf2 website text.txt", "w", encoding="utf-8") as file:
     for line in file:
         stripped_line = line.strip()
         line_list = stripped_line.split()
